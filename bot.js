9be3117b6b712e259756a42702f5a4b0b2ca4603
CHOsen One <37858778+leoncho918@users.noreply.github.com>
2020-04-18 21:04:27 +1000

    Initial commit

---
+++ bot.js
@@ -0,0 +1,109 @@
+var Discord = require("discord.io");
+var logger = require("winston");
+var auth = require("./auth.json");
+var coronavirus = require("./coronavirus");
+const { flag, code, name, countries } = require("country-emoji");
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console(), {
+  colorize: true,
+});
+logger.level = "debug";
+// Initialize Discord Bot
+var bot = new Discord.Client({
+  token: auth.token,
+  autorun: true,
+});
+
+bot.on("ready", function (evt) {
+  logger.info("Connected");
+  logger.info("Logged in as: ");
+  logger.info(bot.username + " - (" + bot.id + ")");
+});
+
+bot.on("message", function (user, userID, channelID, message, evt) {
+  let args = [];
+  console.log(channelID);
+  if (message.includes(" ")) {
+    args.push(message.substr(0, message.indexOf(" ")));
+    args.push(message.substr(message.indexOf(" ") + 1));
+  } else {
+    args.push(message);
+  }
+  var disease = args[0];
+  if (coronavirus.check(disease) && userID !== 700757832848375872) {
+    if (args.length === 1) {
+      coronavirus
+        .worldStats()
+        .then((res) => {
+          const keys = Object.keys(res);
+          let message = "";
+          message = message + "__**Covid-19 Statistics Worldwide 🌏**__\n";
+          for (let i = 0; i < keys.length - 1; i++) {
+            message =
+              message +
+              `**${capitaliseString(
+                keys[i].replace(new RegExp("_", "g"), " ")
+              )}:** ${parseNumber(res[keys[i]])}\n`;
+          }
+          sendMessage(channelID, message);
+        })
+        .catch((e) => {
+          console.log("Error: " + e);
+        });
+    } else if (args.length === 2 && typeof args[1] === "string") {
+      coronavirus.country(args[1]).then((res) => {
+        const keys = Object.keys(res);
+        console.log(res);
+        let message = "";
+        message =
+          message +
+          `__**Covid-19 Statistics for ${
+            res.info.title === "Korea" ? "North Korea" : res.info.title
+          } ${flag(
+            res.info.title === "Korea" ? "North Korea" : res.info.title
+          )}**__\n`;
+        for (let i = 1; i < keys.length; i++) {
+          message =
+            message +
+            `**${capitaliseString(
+              keys[i].replace(new RegExp("_", "g"), " ")
+            )}:** ${parseNumber(res[keys[i]])}\n`;
+        }
+        if (unreliableCountry(res.info.title)) {
+          message =
+            message + "*This country's statistics may not be accurate.*";
+        }
+        sendMessage(channelID, message);
+      });
+    }
+  }
+});
+
+function unreliableCountry(country) {
+  let unreliable = ["china", "russia", "vietnam", "iran", "north korea"];
+  return unreliable.includes(country.toLowerCase());
+}
+
+function capitaliseString(str) {
+  var splitStr = str.toLowerCase().split(" ");
+  for (var i = 0; i < splitStr.length; i++) {
+    // You do not need to check if i is larger than splitStr length, as your for does that for you
+    // Assign it back to the array
+    splitStr[i] =
+      splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+  }
+  // Directly return the joined string
+  return splitStr.join(" ");
+}
+
+function parseNumber(num) {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function sendMessage(channelID, message) {
+  bot.sendMessage({
+    to: channelID,
+    message: message,
+  });
+}
---
+++ coronavirus.js
@@ -0,0 +1,51 @@
+var natural = require("natural");
+const request = require("request");
+const { flag, code, name, countries } = require("country-emoji");
+
+function check(message) {
+  const names = ["covid19", "coronavirus", "cars", "ccpvirus"];
+  for (var i = 0; i < names.length; i++) {
+    if (natural.JaroWinklerDistance(names[i], message) > 0.75) return true;
+  }
+  return false;
+}
+
+function getWorldStats() {
+  return new Promise(function (resolve, reject) {
+    request("https://api.thevirustracker.com/free-api?global=stats", function (
+      error,
+      response,
+      body
+    ) {
+      if (response.statusCode === 200 && JSON.parse(body).results !== undefined)
+        resolve(JSON.parse(body).results[0]);
+      else return error;
+    });
+  });
+}
+
+function getCountryStats(country) {
+  const countryCode = code(country);
+  return new Promise(function (resolve, reject) {
+    request(
+      `https://api.thevirustracker.com/free-api?countryTotal=${countryCode}`,
+      function (error, response, body) {
+        if (
+          response.statusCode === 200 &&
+          JSON.parse(body).countrydata !== undefined
+        ) {
+          resolve(JSON.parse(body).countrydata[0]);
+        } else return error;
+      }
+    );
+  });
+}
+
+function getTopFive() {}
+
+module.exports = {
+  check: check,
+  worldStats: getWorldStats,
+  country: getCountryStats,
+  topFive: getTopFive,
+};
